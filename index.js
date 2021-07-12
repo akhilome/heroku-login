7635be487b9797c31ba2bce3b75eede6cc1c6960
Kizito Akhilome <kizito@akhilo.me>
2021-07-12 12:41:26 +0100

    wip: implement core action functionality

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('scaffold js project');
+const { execSync } = require('child_process');
+const core = require('@actions/core');
+
+const email = core.getInput('heroku_email');
+const apiKey = core.getInput('heroku_api_key');
+
+try {
+  execSync(`cat >~/.netrc <<EOF
+  machine api.heroku.com
+      login ${email}
+      password ${apiKey}
+  machine git.heroku.com
+      login ${email}
+      password ${apiKey}
+  EOF`);
+  core.setOutput('status', 'Logged successfully');
+} catch (error) {
+  core.setOutput('status', 'Unable to login');
+  core.setFailed(error);
+}
