be8a8203cfd2e313b84e043ba51b625ff118bb80
Kizito Akhilome <kizito@akhilo.me>
2024-08-31 14:32:23 +0100

    chore!: change input names

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const { execSync } = require('child_process');
 const core = require('@actions/core');
 
-const email = core.getInput('heroku_email');
-const apiKey = core.getInput('heroku_api_key');
+const email = core.getInput('email');
+const apiToken = core.getInput('api-token');
 
 try {
   execSync(`cat >~/.netrc <<EOF
   machine api.heroku.com
       login ${email}
-      password ${apiKey}
+      password ${apiToken}
   machine git.heroku.com
       login ${email}
-      password ${apiKey}
+      password ${apiToken}
   EOF`);
   core.setOutput('status', 'Logged successfully');
 } catch (error) {
